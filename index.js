88cc3882350078a266c9b589b6667993374aa62a
Christopher Agius <chrisagius@fastmail.com>
2018-04-02 19:49:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function destructivelyRemoveFirstKitten() {
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function appendKitten(name) {
-  return [name, ...kittens];
+  return [...kittens, name];
 }
 
 var kittens = ["Milo", "Otis", "Garfield"]
